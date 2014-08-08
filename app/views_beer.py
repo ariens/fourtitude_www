151e242c93ad00c6861ed5c0b78a29eedda15ae4
Dave Ariens <dave@ariens.ca>
2014-08-07 21:37:18 -0400

    start of template organisation

--- app/views_beer.py
+++ app/views_beer.py
@@ -2,14 +2,15 @@ from flask import render_template
 from app import app
 from .models import Beer
 
+
 @app.route('/beers', methods=['GET', 'POST'])
 def beers():
     """
     This is the route that a user accesses when viewing/managing beers
     :return:
     """
-    beers = Beer.query.all()
+    all_beers = Beer.query.all()
     return render_template(
-        'beers.html',
-        beers=beers,
+        'beers/page.html',
+        beers=all_beers,
         title='Beers')
