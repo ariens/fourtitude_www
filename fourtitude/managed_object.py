00091580ef989f0f08bd7b00e49f8e0c77fcb6d3
Dave Ariens <dave@ariens.ca>
2014-08-22 12:18:39 -0400

    refactored managed object function into stand-alone module

---
+++ fourtitude/managed_object.py
@@ -0,0 +1,110 @@
+from flask import render_template, request, flash, redirect, url_for
+from fourtitude import db
+from fourtitude.forms import ConfirmForm
+
+
+class ManagedObjectException(Exception):
+    pass
+
+
+class ForeignKeyDependency(Exception):
+    pass
+
+
+class ManagedObject():
+
+    @staticmethod
+    def get_auto_manage_label():
+        raise ManagedObjectException("Implementer doesn't support get_auto_manage_label()")
+
+    @staticmethod
+    def manage_template():
+        raise ManagedObjectException("Implementer doesn't support manage_template()")
+
+    @staticmethod
+    def delete_template():
+        raise ManagedObjectException("Implementer doesn't support delete_template()")
+
+    @staticmethod
+    def foreign_key_protected():
+        raise ForeignKeyDependency("Implementer doesn't support foreign_key_protection()")
+
+
+def manage_object(object_registry, object_class, object_id):
+    """
+    Associates a registry of managed object to their class name and form name
+    When an object_id is specified the existing object's form is presented for editing
+    When no object_id is specified a blank form is presented for creating a new object
+    :param object_class: The class name of the managed object
+    :param object_id: The object's id when editing existing
+    :return: Rendered form when editing/creating, redirects upon commit
+    :raise Exception: Any error
+    """
+    if not object_class in object_registry:
+        raise Exception("The object '%s' is not auto-managed" % object_class)
+
+    ManagedClass = object_registry[object_class]['class_name']
+    managed_obj = ManagedClass()
+
+    verb = 'Create'
+    if object_id is not None:
+        verb = 'Update'
+        managed_obj = ManagedClass.query.get(object_id)
+
+    ManagedClassForm = object_registry[object_class]['class_form']
+    form = ManagedClassForm(obj=managed_obj)
+
+    try:
+        if form.validate_on_submit():
+            form.populate_obj(managed_obj)
+            if hasattr(managed_obj, 'form_populate_helper'):
+                managed_obj.form_populate_helper()
+            db.session.add(managed_obj)
+            db.session.commit()
+            flash("Object: '%s' Saved!" % managed_obj.get_auto_manage_label())
+            return redirect(url_for('beer_admin'))
+    except Exception as error:
+        flash(error)
+    return render_template(
+        ManagedClass.manage_template(),
+        title="%s %s" % (verb, managed_obj.get_auto_manage_label()),
+        form=form)
+
+
+def delete_object(object_registry, object_class, object_id):
+    """
+    Associates a registry of managed object to their class name
+    Displays a confirmation before deletion
+    :param object_class: The class name of the managed object
+    :param object_id: The object's id when editing existing
+    :return: Rendered form when confirming, redirects upon commit
+    :raise Exception: Any error
+    """
+    if not object_class in object_registry:
+        raise Exception("The object '%s' is not auto-managed" % object_class)
+
+    ManagedClass = object_registry[object_class]['class_name']
+    managed_obj = ManagedClass.query.get(object_id)
+    form = ConfirmForm()
+    try:
+        if object_id is None:
+            raise Exception("Missing object_id")
+        if request.method == 'POST':
+            if request.form['action'] == 'Cancel':
+                return redirect(url_for('beer_admin'))
+            if request.form['action'] == 'Confirm':
+                if managed_obj.foreign_key_protected():
+                    db.session.delete(managed_obj)
+                    db.session.commit()
+                    flash("%s is gone" % managed_obj)
+                    return redirect(url_for('beer_admin'))
+                else:
+                    flash("We can't delete that because there are related records")
+
+    except Exception as error:
+        flash(error)
+    return render_template(
+        ManagedClass.delete_template(),
+        form=form,
+        managed_obj=managed_obj,
+        title="Please Confirm")
--- fourtitude/views_beer.py
+++ fourtitude/views_beer.py
@@ -1,51 +1,12 @@
-from flask import render_template, request, flash, redirect, url_for
-from fourtitude import app, db, route_restrictions
+from flask import render_template
+from fourtitude import app, route_restrictions
 from fourtitude.models import Beer, BeerStyle, BeerStyleType
-from fourtitude.forms import ConfirmForm, BeerForm, BeerStyleForm, BeerStyleTypeForm
+from fourtitude.forms import BeerForm, BeerStyleForm, BeerStyleTypeForm
+from fourtitude.managed_object import manage_object, delete_object
 
 
-@app.route('/beer', methods=['GET', 'POST'])
-def beers():
-    all_beers = Beer.query.all()
-    all_styles = BeerStyle.query.all()
-    all_style_types = BeerStyleType.query.all()
-    return render_template(
-        'beer/page.html',
-        beers=all_beers,
-        styles=all_styles,
-        style_types=all_style_types,
-        title='Beers')
-
-
-@app.route('/beer/manage', methods=['GET', 'POST'])
-#@route_restrictions.restrict(group_name='beer_admin')
-def beer_admin():
-    all_beers = Beer.query.all()
-    all_styles = BeerStyle.query.all()
-    all_style_types = BeerStyleType.query.all()
-    return render_template(
-        'beer/page.html',
-        admin=True,
-        beers=all_beers,
-        styles=all_styles,
-        style_types=all_style_types,
-        title='Beer Management')
-
-
-@app.route('/beer/manage/<object_class>/', methods=['GET', 'POST'], defaults={'object_id': None})
-@app.route('/beer/manage/<object_class>/<int:object_id>', methods=['GET', 'POST'])
-#@route_restrictions.restrict(group_name='beer_admin')
-def manage_beer_object(object_class, object_id):
-    """
-    Associates a registry of managed object to their class name and form name
-    When an object_id is specified the existing object's form is presented for editing
-    When no object_id is specified a blank form is presented for creating a new object
-    :param object_class: The class name of the managed object
-    :param object_id: The object's id when editing existing
-    :return: Rendered form when editing/creating, redirects upon commit
-    :raise Exception: Any error
-    """
-    auto_manage_registry = {
+def get_beer_registry():
+    return {
         'BeerStyleType': {
             'class_name': BeerStyleType,
             'class_form': BeerStyleTypeForm
@@ -60,86 +21,35 @@ def manage_beer_object(object_class, object_id):
         }
     }
 
-    if not object_class in auto_manage_registry:
-        raise Exception("The object '%s' is not auto-managed" % object_class)
 
-    ManagedClass = auto_manage_registry[object_class]['class_name']
-    managed_obj = ManagedClass()
-
-    verb = 'Create'
-    if object_id is not None:
-        verb = 'Update'
-        managed_obj = ManagedClass.query.get(object_id)
-
-    ManagedClassForm = auto_manage_registry[object_class]['class_form']
-    form = ManagedClassForm(obj=managed_obj)
-
-    try:
-        if form.validate_on_submit():
-            form.populate_obj(managed_obj)
-            if hasattr(managed_obj, 'form_populate_helper'):
-                managed_obj.form_populate_helper()
-            db.session.add(managed_obj)
-            db.session.commit()
-            flash("Object: '%s' Saved!" % managed_obj.get_auto_manage_label())
-            return redirect(url_for('beer_admin'))
-    except Exception as error:
-        flash(error)
+@app.route('/beer', methods=['GET', 'POST'])
+def list_beers(admin=False):
+    all_beers = Beer.query.all()
+    all_styles = BeerStyle.query.all()
+    all_style_types = BeerStyleType.query.all()
     return render_template(
-        ManagedClass.manage_template(),
-        title="%s %s" % (verb, managed_obj.get_auto_manage_label()),
-        form=form)
+        'beer/page.html',
+        admin=admin,
+        beers=all_beers,
+        styles=all_styles,
+        style_types=all_style_types,
+        title='Beers')
+
+
+@app.route('/beer/manage', methods=['GET', 'POST'])
+@route_restrictions.restrict(group_name='beer_admin')
+def beer_admin():
+    return list_beers(admin=True)
+
+
+@app.route('/beer/manage/<object_class>/', methods=['GET', 'POST'], defaults={'object_id': None})
+@app.route('/beer/manage/<object_class>/<int:object_id>', methods=['GET', 'POST'])
+@route_restrictions.restrict(group_name='beer_admin')
+def manage_beer_object(object_class, object_id):
+    return manage_object(get_beer_registry(), object_class, object_id)
 
 
 @app.route('/beer/delete/<object_class>/<int:object_id>', methods=['GET', 'POST'])
-#@route_restrictions.restrict(group_name='beer_admin')
+@route_restrictions.restrict(group_name='beer_admin')
 def delete_beer_object(object_class, object_id):
-    """
-    Associates a registry of managed object to their class name
-    Displays a confirmation before deletion
-    :param object_class: The class name of the managed object
-    :param object_id: The object's id when editing existing
-    :return: Rendered form when confirming, redirects upon commit
-    :raise Exception: Any error
-    """
-    auto_manage_registry = {
-        'BeerStyleType': {
-            'class_name': BeerStyleType,
-        },
-        'BeerStyle': {
-            'class_name': BeerStyle,
-        },
-        'Beer': {
-            'class_name': Beer,
-        }
-    }
-
-    if not object_class in auto_manage_registry:
-        raise Exception("The object '%s' is not auto-managed" % object_class)
-
-    ManagedClass = auto_manage_registry[object_class]['class_name']
-    managed_obj = ManagedClass.query.get(object_id)
-
-    try:
-        form = ConfirmForm()
-        if object_id is None:
-            raise Exception("Missing object_id")
-        if request.method == 'POST':
-            if request.form['action'] == 'Cancel':
-                return redirect(url_for('beer_admin'))
-            if request.form['action'] == 'Confirm':
-                if managed_obj.foreign_key_protected():
-                    db.session.delete(managed_obj)
-                    db.session.commit()
-                    flash("%s is gone" % managed_obj)
-                    return redirect(url_for('beer_admin'))
-                else:
-                    flash("We can't delete that because there are related records")
-
-    except Exception as error:
-        flash(error)
-    return render_template(
-        ManagedClass.delete_template(),
-        form=form,
-        managed_obj=managed_obj,
-        title="Please Confirm")
+    return delete_object(get_beer_registry(), object_class, object_id)
